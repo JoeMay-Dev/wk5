09e8d9c2fb27e80d7c0d368a64eb53eaba69d4b6
Joe May <jmmay1974@gmail.com>
2020-07-05 21:21:36 -0700

    updated UI for past present and future events

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const today = moment().format("dddd, MMMM Do YYYY");
 const now = moment().format("H");
 console.log(today);
@@ -7,45 +6,21 @@ console.log(now);
 $(document).ready(function(){
     $('#currentDate').text(today);
 
-    $("div").each(function (){
+    $("textarea").each(function (){
         let when = $(this).attr("id");
         if (when != undefined) {
-            if (Number(when) < Number(now));
-            $("#" + when).css({"background-color" : "gray", "color" : "white"});
+            if (Number(when) < Number(now)){
+            $("#" + when).addClass("past");
+        }   else if (Number(when) > Number(now)){
+            $("#" + when).addClass("future");
+        }   else {
+            $("#" + when).addClass("present");
+        }
             console.log("#" + when);
             console.log(Number(when));
             console.log(Number(now));
             console.log(Number(when) < Number(now));
-        //  $("#row").css("color", "gray")
         }   
-        // console.log(when);
-        // console.log(when<now);
 
     });
 });
-=======
-const today = moment().format("dddd, MMMM Do YYYY");
-const now = moment().format("H");
-console.log(today);
-console.log(now);
-
-$(document).ready(function(){
-    $('#currentDate').text(today);
-
-    $("div").each(function (){
-        let when = $(this).attr("id");
-        if (when != undefined) {
-            if (Number(when) < Number(now));
-            $("#" + when).css({"background-color" : "gray", "color" : "white"});
-            console.log("#" + when);
-            console.log(Number(when));
-            console.log(Number(now));
-            console.log(Number(when) < Number(now));
-        //  $("#row").css("color", "gray")
-        }   
-        // console.log(when);
-        // console.log(when<now);
-
-    });
-});
->>>>>>> c43938c5041ddb6dd5dd962b89a4f3a15b308910
