6e43b7bae23fbd6d112978217ee53ed3b4cc0045
Joe May <jmmay1974@gmail.com>
2020-07-08 20:40:16 -0700

    removed console logs

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 const today = moment().format("dddd, MMMM Do YYYY");
 const now = moment().format("H");
-console.log(today);
-console.log(now);
-
-
 const saveButton6 = document.getElementById("button6");
 const saveButton7 = document.getElementById("button7");
 const saveButton8 = document.getElementById("button8");
@@ -18,112 +14,95 @@ const saveButton16 = document.getElementById("button16");
 const saveButton17 = document.getElementById("button17");
 const saveButton18 = document.getElementById("button18");
 
-$(document).ready(function(){
+$(document).ready(function () {
     $('#currentDate').text(today);
 
-    $("textarea").each(function (){
+    $("textarea").each(function () {
         let when = $(this).attr("id");
         if (when != undefined) {
-            if (Number(when) < Number(now)){
-            $("#" + when).addClass("past");
-        }   else if (Number(when) > Number(now)){
-            $("#" + when).addClass("future");
-        }   else {
-            $("#" + when).addClass("present");
+            if (Number(when) < Number(now)) {
+                $("#" + when).addClass("past");
+            } else if (Number(when) > Number(now)) {
+                $("#" + when).addClass("future");
+            } else {
+                $("#" + when).addClass("present");
+            }
         }
-            console.log("#" + when);
-            console.log(Number(when));
-            console.log(Number(now));
-            console.log(Number(when) < Number(now));
-        }   
 
     });
 });
 
-for (let i=6; i<=18; i++){
-    let checkTask = localStorage.getItem("task"+ i);
+for (let i = 6; i <= 18; i++) {
+    let checkTask = localStorage.getItem("task" + i);
     let renderTask = document.getElementById([i]);
 
     if ((checkTask) != "") {
         renderTask.textContent = checkTask;
     }
-    };
+};
 
-saveButton6.addEventListener("click", function(event){
+saveButton6.addEventListener("click", function (event) {
     event.preventDefault();
-        const eventInput = document.getElementById("6").value;
-    console.log(eventInput);
-    localStorage.setItem("task6", eventInput);
+    const inputTask = document.getElementById("6").value;
+    localStorage.setItem("task6", inputTask);
 });
-saveButton7.addEventListener("click", function(event){
+saveButton7.addEventListener("click", function (event) {
     event.preventDefault();
-        const eventInput = document.getElementById("7").value;
-    console.log(eventInput);
-    localStorage.setItem("task7", eventInput);
+    const inputTask = document.getElementById("7").value;
+    localStorage.setItem("task7", inputTask);
 });
-saveButton8.addEventListener("click", function(event){
+saveButton8.addEventListener("click", function (event) {
     event.preventDefault();
-        const eventInput = document.getElementById("8").value;
-    console.log(eventInput);
-    localStorage.setItem("task8", eventInput);
+    const inputTask = document.getElementById("8").value;
+    localStorage.setItem("task8", inputTask);
 });
-saveButton9.addEventListener("click", function(event){
+saveButton9.addEventListener("click", function (event) {
     event.preventDefault();
-        const eventInput = document.getElementById("9").value;
-    console.log(eventInput);
-    localStorage.setItem("task9", eventInput);
+    const inputTask = document.getElementById("9").value;
+    localStorage.setItem("task9", inputTask);
 });
-saveButton10.addEventListener("click", function(event){
+saveButton10.addEventListener("click", function (event) {
     event.preventDefault();
-        const eventInput = document.getElementById("10").value;
-    console.log(eventInput);
-    localStorage.setItem("task10", eventInput);
+    const inputTask = document.getElementById("10").value;
+    localStorage.setItem("task10", inputTask);
 });
-saveButton11.addEventListener("click", function(event){
+saveButton11.addEventListener("click", function (event) {
     event.preventDefault();
-        const eventInput = document.getElementById("11").value;
-    console.log(eventInput);
-    localStorage.setItem("task11", eventInput);
+    const inputTask = document.getElementById("11").value;
+    localStorage.setItem("task11", inputTask);
 });
-saveButton12.addEventListener("click", function(event){
+saveButton12.addEventListener("click", function (event) {
     event.preventDefault();
-        const eventInput = document.getElementById("12").value;
-    console.log(eventInput);
-    localStorage.setItem("task12", eventInput);
+    const inputTask = document.getElementById("12").value;
+    localStorage.setItem("task12", inputTask);
 });
-saveButton13.addEventListener("click", function(event){
+saveButton13.addEventListener("click", function (event) {
     event.preventDefault();
-        const eventInput = document.getElementById("13").value;
-    console.log(eventInput);
-    localStorage.setItem("task13", eventInput);
+    const inputTask = document.getElementById("13").value;
+    localStorage.setItem("task13", inputTask);
 });
-saveButton14.addEventListener("click", function(event){
+saveButton14.addEventListener("click", function (event) {
     event.preventDefault();
-        const eventInput = document.getElementById("14").value;
-    console.log(eventInput);
-    localStorage.setItem("task14", eventInput);
+    const inputTask = document.getElementById("14").value;
+    localStorage.setItem("task14", inputTask);
 });
-saveButton15.addEventListener("click", function(event){
+saveButton15.addEventListener("click", function (event) {
     event.preventDefault();
-        const eventInput = document.getElementById("15").value;
-    console.log(eventInput);
-    localStorage.setItem("task15", eventInput);
+    const inputTask = document.getElementById("15").value;
+    localStorage.setItem("task15", inputTask);
 });
-saveButton16.addEventListener("click", function(event){
+saveButton16.addEventListener("click", function (event) {
     event.preventDefault();
-        const eventInput = document.getElementById("16").value;
-    console.log(eventInput);
-    localStorage.setItem("task16", eventInput);
+    const inputTask = document.getElementById("16").value;
+    localStorage.setItem("task16", inputTask);
 });
-saveButton17.addEventListener("click", function(event){
+saveButton17.addEventListener("click", function (event) {
     event.preventDefault();
-        const eventInput = document.getElementById("17").value;
-    console.log(eventInput);
-    localStorage.setItem("task17", eventInput);
+    const inputTask = document.getElementById("17").value;
+    localStorage.setItem("task17", inputTask);
 });
-saveButton18.addEventListener("click", function(event){
+saveButton18.addEventListener("click", function (event) {
     event.preventDefault();
-        const eventInput = document.getElementById("18").value;
-    console.log(eventInput);
-    localStorage.setItem("task18", eventInput);
+    const inputTask = document.getElementById("18").value;
+    localStorage.setItem("task18", inputTask);
 });
